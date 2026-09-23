0413742a81f91c9bf8fbb2af5fa1e75296d3be31
Anatoly Lebedev <anatoly@lbdv.dev>
2026-09-23 03:10:53 +0200

    the bug in continuously filling yes to the bash call, caught with `pipefail`

--- provisioners/install-zfs.sh
+++ provisioners/install-zfs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 echo "=== Install ZFS on Debian ==="
-yes | bash /root/.oldroot/nfs/tools/install_openzfs.sh
+{ yes || true; } | bash /root/.oldroot/nfs/tools/install_openzfs.sh
 echo "=== ZFS Kernel Module Load ==="
 modprobe zfs
 echo "=== Force Kernel to update on partition table changes ==="
